9dffd8e20a3e09488da8710aec496c6bc7291026
Ilija Stanojević <stanojevic.sv71.2020@uns.ac.rs>
2023-05-02 15:01:32 +0200

    Init spring project

---
+++ Cloud-Tim-15/src/main/java/com/example/CloudTim15/CloudTim15Application.java
@@ -0,0 +1,13 @@
+package com.example.CloudTim15;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudTim15Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudTim15Application.class, args);
+	}
+
+}
---
+++ Cloud-Tim-15/src/test/java/com/example/CloudTim15/CloudTim15ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CloudTim15;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudTim15ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
