59b45e8ec28089936c49d488c486f6ca7667d167
Ilija Stanojević <stanojevic.sv71.2020@uns.ac.rs>
2023-05-04 13:23:03 +0200

    Add page for uploading files

--- Cloud-Tim-9/src/app/componenets/home/home.component.ts
+++ Cloud-Tim-9/src/app/componenets/home/home.component.ts
@@ -1,36 +1,65 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { CognitoService } from 'src/app/services/cognito.service';
+import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
+import {CognitoService} from 'src/app/services/cognito.service';
+import {HttpClient, HttpHeaders, HttpRequest, HttpResponse} from "@angular/common/http";
+import {catchError, filter} from "rxjs";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit{
+export class HomeComponent implements OnInit {
+  protected selectedFile: File | null = null;
 
-  constructor(private router:Router, private cognitoService: CognitoService) {}
+  constructor(private router: Router, private cognitoService: CognitoService, private http: HttpClient) {
+  }
 
-  ngOnInit(): void{
+  ngOnInit(): void {
     this.getUserDetails();
   }
 
-  private getUserDetails(){
+  private getUserDetails() {
     this.cognitoService.getUser()
-    .then((user:any) => {
-      if (user) {
+      .then((user: any) => {
+        if (user) {
 
-      }
-      else{
-        this.router.navigate(['/sign-in']);
-      }
-    })
+        } else {
+          this.router.navigate(['/sign-in']);
+        }
+      })
   }
 
-  signOutWithCognito(){
+  signOutWithCognito() {
     this.cognitoService.signOut()
-    .then(() => {
-      this.router.navigate(['/sign-in'])
-    })
+      .then(() => {
+        this.router.navigate(['/sign-in'])
+      })
   }
+
+  handleFileInput(event: any) {
+    this.selectedFile = event.target.files[0] as File;
+  }
+
+  onUpload() {
+    if (this.selectedFile) {
+      const formData: FormData = new FormData();
+      formData.append('file', this.selectedFile);
+      console.log(this.selectedFile.name);
+      console.log(formData);
+      this.http.put('https://0a4gl4t68d.execute-api.us-east-1.amazonaws.com/KT1/cloud-tim9-mediafiles/test5.png', formData)
+        .subscribe(
+          response => {
+            console.log(response);
+            // Handle the response
+          },
+          error => {
+            console.log(error);
+            // Handle the error
+          }
+        );
+    }
+  }
+
+
 }
