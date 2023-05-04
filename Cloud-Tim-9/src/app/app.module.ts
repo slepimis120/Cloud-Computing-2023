f11b5abdd7fc179157b11b4284ab49e7a8f4298e
REX1389 <rrvlelcc@gmail.com>
2023-05-04 09:25:30 +0200

    Hopefully finished the sign in.

--- Cloud-Tim-9/src/app/app.module.ts
+++ Cloud-Tim-9/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { SignInComponent } from './componenets/sign-in/sign-in.component';
 import { SignUpComponent } from './componenets/sign-up/sign-up.component';
 import { AppRoutingModule } from './app-routing.module';
 import { MessageModalComponent } from './componenets/message-modal/message-modal.component';
@@ -11,7 +12,7 @@ import { HomeComponent } from './componenets/home/home.component';
 @NgModule({
   declarations: [
     AppComponent,
-    SignUpComponent,
+    SignInComponent,
     SignUpComponent,
     MessageModalComponent,
     HomeComponent
--- Cloud-Tim-9/src/app/componenets/sign-in/sign-in.component.ts
+++ Cloud-Tim-9/src/app/componenets/sign-in/sign-in.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Router} from "@angular/router";
+import {CognitoService} from "../../services/cognito.service";
+import {User} from "../../models/user";
 
 @Component({
   selector: 'app-sign-in',
   templateUrl: './sign-in.component.html',
   styleUrls: ['./sign-in.component.css']
 })
-export class SignInComponent {
-
+export class SignInComponent implements OnInit
+{
+  user: User | undefined;
+  alertMessage: string = "";
+  showAlert: boolean = false;
+  
+  isForgotPassword: boolean = false;
+  newPassword: string = "";
+  
+  constructor(private router:Router, private cognitoService : CognitoService) {}
+  
+  ngOnInit()
+  {
+    this.user = {} as User;
+  }
+  
+  SignIn()
+  {
+    if(this.user && this.user.email && this.user.password)
+      this.cognitoService.signIn(this.user)
+      .then(() =>
+      {
+        this.router.navigate(["/"]);
+      })
+      .catch((error: any) =>
+      {
+        this.displayAlert(error.message);
+      });
+    else
+      this.displayAlert("E-mail or password not valid.");
+  }
+  
+  private displayAlert(message:string)
+  {
+    this.alertMessage = message;
+    this.showAlert = true;
+  }
 }
